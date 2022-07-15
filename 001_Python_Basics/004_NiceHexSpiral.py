3addea3d96a5b88ded3440dd3aaa522252206ae8
pochiman <pochiman@yahoo.com>
2022-07-14 23:44:15 -0400

    first commit

--- 001_Python_Basics/004_NiceHexSpiral.py
+++ 001_Python_Basics/004_NiceHexSpiral.py
@@ -1,8 +1,8 @@
 # NiceHexSpiral.py
 import turtle
-colors=['red', 'purple', 'blue',
+colors = ['red', 'purple', 'blue',
         'green', 'yellow', 'orange']
-t=turtle.Pen()
+t = turtle.Pen()
 turtle.bgcolor('black')
 for x in range(360):
     t.pencolor(colors[x%6])
---
+++ 002_Turtle_Graphics/001_SquareSpiral1.py
@@ -0,0 +1,6 @@
+# SquareSpiral1.py - Draws a square spiral
+import turtle
+t = turtle.Pen()
+for x in range(100):
+    t.forward(x)
+    t.left(90)
